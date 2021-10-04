f7eeafdc23ed899b993e6d3670e96f4ed918370c
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2021-10-04 22:22:12 +0500

    Hacktoberfest
    Updating

--- Task_1.js
+++ Task_1.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     if ($(this).val() == '') {
       $(this).parent().find(".label-txt").removeClass('label-active');
       console.log("KEHRA HAL THAI");
+      console.log("Work in progress!");
     };
   });
 
