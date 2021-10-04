f2808a8a49970f5fbcd8bee0bbb45730bad09ee5
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2021-10-04 19:01:13 +0500

    Hacktoberfest
    IMU SAMA

--- Task_1.js
+++ Task_1.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   $(".input").focusout(function(){
     if ($(this).val() == '') {
       $(this).parent().find(".label-txt").removeClass('label-active');
+      console.log("KEHRA HAL THAI");
     };
   });
 
