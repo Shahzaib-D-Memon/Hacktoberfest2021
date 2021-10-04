d7187387c2ebb207013299cef5e22a503f96cc9d
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2021-10-04 19:54:43 +0500

    Hacktoberfest
    Tincture

--- Task_3.php
+++ Task_3.php
@@ -2,7 +2,7 @@
 <html>
 
 	<head>
-		<title>Student portal</title>
+		<title>Student Portal</title>
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
@@ -15,11 +15,11 @@
 			body{
 				margin-top:70px;
 				background:gray;
-				color:yellow;
+				color:orange;
 			}
 			
 			card card-primary mb-2{
-				background-color: blue;
+				background-color: green;
 			}
 		</style>
 		
